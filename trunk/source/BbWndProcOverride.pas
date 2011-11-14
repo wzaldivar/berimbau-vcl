3f176266d40203ce502ba5c637f0e89f26f4ef0a
wzaldivar <wzaldivar@a0639243-de7b-49f1-b4ba-acd9768d4d0f>
2011-11-14 21:07:12 +0000

    description added

--- trunk/source/BbWndProcOverride.pas
+++ trunk/source/BbWndProcOverride.pas
@@ -1,3 +1,30 @@
+{*******************************************************************************
+
+  Berimbau VCL
+
+  BbWndProcOverride.pas
+
+  Provides interfaces for overriding windows procedures
+
+  Copyright (C) 2009 Walber Zaldivar <walber.zaldivar@gmail.com>
+
+********************************************************************************
+
+  This library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************}
+
 unit BbWndProcOverride;
 
 interface
